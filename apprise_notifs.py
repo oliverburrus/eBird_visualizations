0febe8d81bd37acf66f8323412e5108dd21d0c42
oliverburrus <60049181+oliverburrus@users.noreply.github.com>
2022-07-08 22:09:43 -0500

    Create apprise_notifs.py

---
+++ apprise_notifs.py
@@ -0,0 +1,5 @@
+# Project statement:
+#   Upon completion of this project, I will atempt to achieve the following:
+#     Notify via Discord of rare birds reported on eBird
+#     Give weekly regional statistics
+#     Predict where the best birding will be for the following day based on historical records and forcasted weather.
